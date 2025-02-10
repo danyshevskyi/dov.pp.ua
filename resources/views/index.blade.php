e7f8da5a48fd05dfd482d956e7942ebbbb8ae188
Oleksii Danyshevskyi <danishevskij@gmail.com>
2025-02-10 14:49:53 +0200

    Fix display favicon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
     <title>Oleksii Danyshevskyi - PHP Developer || PHP, LARAVEL, JS, MYSQL, HTML, CSS</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" integrity="sha384-4LISF5TTJX/fLmGSxO53rV4miRxdg84mZsxmO8Rx5jGtp/LbrixFETvWa5a6sESd" crossorigin="anonymous">
-    <link rel="icon" type="image/x-icon" href="/index/App/Views/img/favicon.ico"/>
+    <link rel="icon" type="image/x-icon" href="/public/favicon.ico"/>
 <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>  
 <style type="text/css">     
     body {
