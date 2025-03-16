e515393ea0e82046383345d9297ed4709d677fff
Oleksii Danyshevskyi <danishevskij@gmail.com>
2025-03-16 18:40:52 +0200

    Set CORS for frontend

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,13 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use Analytics\App\Models\Count;
 
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+
+
 Route::get('/', function () {
     // return view('welcome');
 
