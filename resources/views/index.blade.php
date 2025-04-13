3b9dc1ca0f17c83c314eef42c514913c99d4cef4
Oleksii Danyshevskyi <danishevskij@gmail.com>
2025-04-13 16:38:01 +0300

    Fix - Create main page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Oleksii Danyshevskyi - PHP Developer || PHP, LARAVEL, JS, MYSQL, HTML, CSS</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" integrity="sha384-4LISF5TTJX/fLmGSxO53rV4miRxdg84mZsxmO8Rx5jGtp/LbrixFETvWa5a6sESd" crossorigin="anonymous">
+    <link rel="icon" type="image/x-icon" href="/public/favicon.ico"/>
+<script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>  
+<style type="text/css">     
+    body {
+        background-color: #F4F1EB;
+    }
+    .vertical-center {
+        min-height: 100%;  /* Fallback for browsers do NOT support vh unit */
+        min-height: 60vh; /* These two lines are counted as one :-)       */
+        display: flex;
+        align-items: center;
+    }
+    .bb {
+        border: 1px solid blue;
+    }
+</style>
+</head>
+
+<body>
+
+<div class="container" style="max-width: 1000px">
+    <div class="wrapper">
+        <div class="top">
+            <div class="col py-2 1bb">
+
+                <nav class="navbar navbar-expand-lg 1bg-body-tertiary">
+                <div class="container-fluid">
+
+                    <div class="dropdown navbar-toggler border-0 1bb ps-0 ms-0">
+                        <!-- <button class="btn btn-outline-black dropdown-toggle border-0 ps-0" type="button"
+                         data-bs-toggle="dropdown" aria-expanded="false">
+                        Projects
+                        </button> -->
+                        <button type="button" class="btn btn-outline-dark dropdown-toggle"
+                        data-bs-toggle="dropdown" aria-expanded="false">Projects</button>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item py-3" href="https://dov.pp.ua/analytics/" target=_blank>DOV Analytics    </a></li>
+                            <li><a class="dropdown-item py-3" href="https://dov.pp.ua/english-cards/" target=_blank>English Cards</a></li>
+                            <li><a class="dropdown-item py-3" href="https://dov.pp.ua/miles/" target=_blank>Miles</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
+                        <li class="nav-item">
+                        <a class="nav-link text-black" href="https://dov.pp.ua/analytics/" target=_blank>DOV Analytics</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link text-black" href="https://dov.pp.ua/english-cards/" target=_blank>English Cards</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link text-black" href="https://dov.pp.ua/miles/" target=_blank>Miles</a>
+                        </li>
+                        </ul>
+                    </div>
+                    <!-- <button type="button" class="btn btn-outline-dark">Projects</button> -->
+                    <a class="me-2 text-black text-decoration-none" href='http://localhost:8000/login'>Log in</a>
+                </div>
+                </nav>
+            </div>
+        </div>
+
+<div class="py-3"></div>
+
+<!-- Content -->
+<a href="https://dov.pp.ua" class="text-decoration-none text-black">
+    <div class="jumbotron vertical-center"> 
+        <div class="container">
+            <div class="text-center mx-auto">
+            <h1 style="font-size: 40px;">Oleksii Danyshevskyi</h1>
+                </div>
+    <h5 class="text-center"><i>PHP Developer</i></h5>
+</a>   
+        </div>
+    </div>
+</div>
+<!-- /Content -->
+
+<!-- footer -->
+<div class="d-flex justify-content-center mt-5 1bb">
+
+  <div class="mb-4 fixed-bottom 1bb text-center 1bb">
+  <a href="https://www.linkedin.com/in/danyshevskyi/" target="_blank" class="text-black">  
+    <i class="bi bi-linkedin fs-4 1me-3"></i></a>
+        <span class="px-3"></span>
+    <a href="https://github.com/oleksiidan" target="_blank" class="text-black">
+        <i class="bi bi-github fs-4"></i>
+    </a>
+       <br><br>
+    <a href="mailto:danishevskij@gmail.com" class="text-decoration-none text-black 1fs-5">danishevskij@gmail.com</a>
+    </div>
+</div>
+<!-- /footer -->
+
+    </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</body>
+</html>
