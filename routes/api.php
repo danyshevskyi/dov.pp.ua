1e44a8d08c3bae50f33b2efa5c4ed2ac7e8e0802
Oleksii Danyshevskyi <danishevskij@gmail.com>
2025-09-06 10:13:15 +0300

    Fix timezone

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,8 @@
 <?php
 
-use App\Http\Controllers\ScodeController;
 use App\Http\Controllers\FeedbackController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Analytics\App\Models\Count;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -12,31 +10,4 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/feedback', [FeedbackController::class, 'create_feedback']);
 
-// *** Scode project ***
-Route::post('/scode', function(Request $request) { 
-    // DOV Analytics
-    $count = new Count('scode', 'all');
-        $count->count();
-
-    $ScodeController = new ScodeController();
-        return $ScodeController->search($request);
-});
-
-Route::post('/scode/all', function() {
-    // DOV Analytics
-    $count = new Count('scode', 'all');
-        $count->count();
-
-    $ScodeController = new ScodeController();
-        return $ScodeController->getAllScodes();
-});
-
-Route::post('/scode/stacker/components', function() {
-    // DOV Analytics
-    // $count = new Count('scode', 'all');
-    //     $count->count();
-
-    $ScodeController = new ScodeController();
-        return $ScodeController->getStackerComponents();
-});
-// /*** Scode project ***
+require_once 'scode.php';
