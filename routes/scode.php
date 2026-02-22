ddde737921bc98412c67364a6a3e7c7e4a7b90fd
Oleksii Danyshevskyi <danishevskij@gmail.com>
2026-02-22 17:11:03 +0200

    Add routes for analytics

--- routes/scode.php
+++ routes/scode.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ScodeController;
 
 Route::post('/scode/open_app', [ScodeController::class, 'analytics']);
+Route::post('/scode/add_desktop', [ScodeController::class, 'analytics']);
+Route::post('/scode/about', [ScodeController::class, 'analytics']);
+Route::post('/scode/donats', [ScodeController::class, 'analytics']);
 Route::post('/scode/search', [ScodeController::class, 'search']);
 Route::post('/scode/scodes_all', [ScodeController::class, 'getAllScodes']);
 Route::post('/scode/stacker', [ScodeController::class, 'getStackerComponents']);
